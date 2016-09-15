561772c2f8fdedc1411e3e0114edccffd2343349
brandonbaah <brandonbaah@gmail.com>
2016-09-15 19:21:21 -0400

    added login action users controller, create route and view for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def login
+  end
+
+  def create
+    @user = User.create(
+      email: params[:email],
+      password_digest: params[:password]
+    )
+  end
 end
