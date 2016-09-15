d01a73ce036f049fd073e8ac5967cb801b9fe217
brandonbaah <brandonbaah@gmail.com>
2016-09-15 19:42:35 -0400

    created home and resume views with appropriate actions and links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,16 @@ class UsersController < ApplicationController
   def login
   end
 
+  def home
+  end
+
   def create
     @user = User.create(
       email: params[:email],
       password_digest: params[:password]
     )
+  end
+
+  def resume
   end
 end
